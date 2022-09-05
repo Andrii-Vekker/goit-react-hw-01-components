35fabc0340bd75065d785bc6fe2a73844b50c704
Andrii-Vekker <vekker304@gmail.com>
2022-09-05 21:49:12 +0300

    wdwd

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -5,7 +5,7 @@ export default function Statistics({dataStat}) {
         <section className="statistics">
             <h2 className="title">Upload stats</h2>
             <ul className="stat-list">
-                {dataStat.map(i => (
+                {dataStat.map(i => ( 
                     <li className="item" key={i.id}>
                         <span className="label">{i.label}</span>
                         <span className="percentage">{i.percentage}</span>
